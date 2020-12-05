4e87cf1b6d39e3859f477a644b95007df35cf2bc
Joshua Young <djry1999@gmail.com>
2020-12-05 15:52:50 +1000

    Install selenium and test login

--- extractor.py
+++ extractor.py
@@ -1,4 +1,19 @@
 from _csv import reader
+from selenium import webdriver
+
+
+def bb_login():
+    driver = webdriver.Safari()
+    driver.get('https://esoe.qut.edu.au/auth/realms/qut/protocol/openid-connect/auth?response_type=code&client_id=shibboleth-2-idp&redirect_uri=https%3A%2F%2Fidp.qut.edu.au%2Fidp%2Fprofile%2FSAML2%2FPOST%2FSSO?execution%3De5s1%26_eventId_proceed%3D1&state=228096%2F235063b5-dfc0-4ddc-b105-b4ac85010379&scope=openid')
+
+    username = driver.find_element_by_id('username')
+    password = driver.find_element_by_id('password')
+
+    username.send_keys('n10404074')
+    password.send_keys('Naruto19ro99%')
+
+    login = driver.find_element_by_xpath('//*[@id="kc-login"]')
+    login.click()
 
 
 def extract_timetable(timetable_file):
@@ -9,3 +24,7 @@ def extract_timetable(timetable_file):
             rows.append(row)
 
     return rows
+
+
+if __name__ == '__main__':
+    bb_login()
--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ OPTIONS = {
         'CFBundleShortVersionString': '1.0.0',
         'LSUIElement': True,
     },
-    'packages': ['_csv', 'PyQt5', 'rumps']
+    'packages': ['_csv', 'selenium', 'PyQt5', 'rumps']
 }
 
 setup(
     app=APP,
     name='MyQUT',
     options={'py2app': OPTIONS},
-    setup_requires=['py2app'], install_requires=['_csv', 'PyQt5', 'rumps']
+    setup_requires=['py2app'], install_requires=['_csv', 'selenium', 'PyQt5', 'rumps']
 )
